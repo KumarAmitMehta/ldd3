b065e17644506f6a60327182e7c558bdcb34d5e8
KumarAmitMehta <gmate.amit@gmail.com>
2012-10-18 23:37:21 -0700

    Fixing more compilation errors
    Signed-off-by: Kumar Amit Mehta <gmate.amit@gmail.com>

--- misc-progs/polltest.c
+++ misc-progs/polltest.c
@@ -27,24 +27,25 @@ char buffer[4096];
 
 int main(int argc, char **argv)
 {
-    struct pollfd pfd;
-    int n;
+        struct pollfd pfd;
+        int n;
 
-    fcntl(0, F_SETFL, fcntl(0,F_GETFL) | O_NONBLOCK); /* stdin */
-    pfd.fd = 0;  /* stdin */
-    pfd.events = POLLIN;
+        fcntl(0, F_SETFL, fcntl(0,F_GETFL) | O_NONBLOCK); /* stdin */
+        pfd.fd = 0;  /* stdin */
+        pfd.events = POLLIN;
 
-    while (1) {
-        n=read(0, buffer, 4096);
-        if (n >= 0)
-            if (write(1, buffer, n) < 0) {
-		perror("write error");
-		exit (errno);
-	    }
-	n = poll(&pfd, 1, -1);
-	if (n < 0)
-	    break;
-    }
-    perror( n<0 ? "stdin" : "stdout");
-    exit(1);
+        while (1) {
+                n=read(0, buffer, 4096);
+                if (n >= 0) {
+                        if (write(1, buffer, n) < 0) {
+                                perror("write error");
+                                exit (errno);
+                        }
+                }
+                n = poll(&pfd, 1, -1);
+                if (n < 0)
+                        break;
+        }
+        perror( n<0 ? "stdin" : "stdout");
+        exit(1);
 }
